4ac8d9e71eb86926c59f98fb3b365c1e245cdf41
Runcel <nevorochka@gmail.com>
2024-05-05 17:24:32 +0700

    sum tests upgrade

--- src/tests/s21_test_sum_matrix.c
+++ src/tests/s21_test_sum_matrix.c
@@ -20,16 +20,16 @@ START_TEST(sum_normal_test){
 
     s21_sum_matrix(&A, &B, &res);
 
+    printf("\n_________\n");
     print_matrix(&A);
     print_matrix(&B);
     print_matrix(&res);
-
+    
     for (int i = 0; i < A.rows; i++){
-        for (int j = 0; j < B.columns; j++){
+        for (int j = 0; j < A.columns; j++){
             actual = res.matrix[i][j];
             expected = A.matrix[i][j] + B.matrix[i][j];
             ck_assert_double_eq(expected, actual);
-            printf("\n_________\n");
             printf("actual:%lf expected:%lf\n", actual, expected);
         }
     }
@@ -37,39 +37,54 @@ START_TEST(sum_normal_test){
     
     printf("\n_________\n");
 
-    
+    s21_remove_matrix(&res);    
     s21_remove_matrix(&A);
     s21_remove_matrix(&B);
 }
 
-// static int rows_edge[] = {5, 0, -1, 3, 5, 5};
-// static int cols_edge[] = {0, 0, 5, 3, 4, 5};
+static int rows_edge[] = {5, 0, 3, 5, 5};
+static int cols_edge[] = {0, 0, 3, 4, 5};
 
-// static double A_filler_edge[] = {1, 0, 1.00000001, 324.4123142, 0, 1};
-// static double B_filler_edge[] = {1, 0, 0.00000001, 324.4123143, 0.000001, 1};
+static double A_filler_edge[] = {1, 1, 0, 0, 0};
+static double B_filler_edge[] = {1, 1, 0, 0.00000001, -0};
 
-// START_TEST(sum_edge_test){
-//     if (_i == 0){
-//         printf("\n\t______________\n");
-//         printf("\tEDGE_VALS");
-//         printf("\n\t______________\n");
-//     }
+START_TEST(sum_edge_test){
+    if (_i == 0){
+        printf("\n\t______________\n");
+        printf("\tEDGE_VALS");
+        printf("\n\t______________\n");
+    }
 
-//     matrix_t A, B;
-//     s21_create_matrix(rows_edge[_i], cols_edge[_i], &A);
-//     s21_create_matrix(rows_edge[_i], cols_edge[_i], &B);
+    matrix_t A, B, res;
+    double actual, expected;
+    s21_create_matrix(rows_edge[_i], cols_edge[_i], &A);
+    s21_create_matrix(rows_edge[_i], cols_edge[_i], &B);
 
-//     fill_matrix(&A, A_filler_edge[_i]);
-//     fill_matrix(&B, B_filler_edge[_i]);
+    fill_matrix(&A, A_filler_edge[_i]);
+    fill_matrix(&B, B_filler_edge[_i]);
 
-   
-//     printf("\n_________\n");
-//     printf("\n_________\n");
+    s21_sum_matrix(&A, &B, &res);
 
+    printf("\n_________\n");
+    print_matrix(&A);
+    print_matrix(&B);
+    print_matrix(&res);
 
-//     s21_remove_matrix(&A);
-//     s21_remove_matrix(&B);
-// }
+    for (int i = 0; i < A.rows; i++){
+        for (int j = 0; j < A.columns; j++){
+            actual = res.matrix[i][j];
+            expected = A.matrix[i][j] + B.matrix[i][j];
+            ck_assert_double_eq(expected, actual);
+            printf("actual:%lf expected:%lf\n", actual, expected);
+        }
+    }
+
+    printf("\n_________\n");
+
+    s21_remove_matrix(&res);   
+    s21_remove_matrix(&A);
+    s21_remove_matrix(&B);
+}
 
 
 Suite *suite_s21_sum_matrix(){
@@ -79,9 +94,9 @@ Suite *suite_s21_sum_matrix(){
     tcase_add_loop_test(normal_case, sum_normal_test, 0, sizeof(rows_normal) / sizeof(int));
     suite_add_tcase(suite, normal_case);
 
-    // TCase *edge_case = tcase_create("case_s21_sum_matrix_edge");
-    // tcase_add_loop_test(edge_case, sum_edge_test, 0, sizeof(rows_edge) / sizeof(int));
-    // suite_add_tcase(suite, edge_case);
+    TCase *edge_case = tcase_create("case_s21_sum_matrix_edge");
+    tcase_add_loop_test(edge_case, sum_edge_test, 0, sizeof(rows_edge) / sizeof(int));
+    suite_add_tcase(suite, edge_case);
 
 
     return suite;
